6a33111778b5e686dcdce06b173adc5baa5ff681
YicunWendyWu9f <a13133925311@sina.com>
2021-02-25 08:51:42 +0800

    [ENH] week5 c# training

---
+++ week5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace week5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Type your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Type your surname:");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba "+name+" "+ surname);
+
+        }
+
+    }
+}
